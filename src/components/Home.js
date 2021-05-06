65c7e47459ba3866b154cb7e4c0ec249f7f75a38
Veto Ramirez <vetorobert3@gmail.com>
2021-05-05 20:26:43 -0700

    Added my rmdb project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {
-  API_URL, 
-  API_KEY,
+  POPULAR_BASE_URL,
+  SEARCH_BASE_URL,
   POSTER_SIZE,
   IMAGE_BASE_URL,
   BACKDROP_SIZE
@@ -31,9 +31,16 @@ const Home = () => {
   ]= useHomeFetch();
   const [searchTerm, setSearchTerm] = useState('');
 
+  const searchMovies = search => {
+    const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+    setSearchTerm(search);
+    fetchMovies(endpoint);
+  }
+
   const loadMoreMovies = () => {
-    const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`;
-    const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`;
+    const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`;
+    const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
 
     const endPoint = searchTerm ? searchEndpoint : popularEndpoint;
 
@@ -45,31 +52,33 @@ const Home = () => {
   
   return (
     <>
-    <HeroImage
-      image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-      title={heroImage.original_title}
-      text={heroImage.overview}
-    />
-    <SearchBar />
-    <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
-      {movies.map(movie => (
-        <MovieThumb
-          key={movie.id}
-          clickable
-          image={
-            movie.poster_path 
-              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-              : NoImage
-          }
-          movieId={movie.id}
-          movieName={movie.original_title}
+      {!searchTerm && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+          title={heroImage.original_title}
+          text={heroImage.overview}
         />
-      ))}
-    </Grid>
-    {loading && <Spinner />}
-    {currentPage < totalPages && !loading && (
-      <LoadMoreBtn text='Load More' callBack={loadMoreMovies} />
-    )}  
+      )}   
+      <SearchBar callback={searchMovies} />
+      <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
+        {movies.map(movie => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path 
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
+      {loading && <Spinner />}
+      {currentPage < totalPages && !loading && (
+        <LoadMoreBtn text='Load More' callBack={loadMoreMovies} />
+      )}  
   </>
   )
 }
--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import FontAwesome from 'react-fontawesome';
 
 import { 
@@ -6,7 +6,21 @@ import {
   StyledSearchBarContent 
 } from '../styles/StyledSearchBar';
 
-const SearchBar = () => {
+const SearchBar = ({ callback }) => {
+  const [state, setState] = useState('');
+  const timeOut = useRef(null);
+
+  const doSearch = event => {
+    const { value } = event.target;
+    
+    clearTimeout(timeOut.current);
+    setState(value);
+
+    timeOut.current = setTimeout(() => {
+      callback(value);
+    }, 500);
+  }
+
   return (
     <StyledSearchBar>
       <StyledSearchBarContent>
